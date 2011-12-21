853e2f5358c621d23d100b28103dce56ddb72873
Yarik Smirnov <yaroslav.smirnov@e-legion.com>
2011-12-22 02:56:59 +0400

    change prefix YS -> EL

---
+++ ELPickerView/ELPickerView.h
@@ -0,0 +1,35 @@
+//
+//  YSPickerView.h
+//  
+//
+//  Created by Yarik Smirnov on 12/12/11.
+//  Copyright (c) 2011 e-Legion ltd. All rights reserved.
+//
+
+@class ELPickerView;
+@protocol ELPickerViewDelegate <NSObject>
+
+- (void)pickerView:(YSPickerView *)pickerView didSelectValue:(NSInteger)value;
+
+- (void)pickerViewWillBeginScrollingAnimation;
+- (void)pickerViewDidiEndScrollingAnimation;
+
+@end
+
+@interface ELPickerView : UIView {
+@private
+    UIScrollView *_scrollView;
+    id<UIScrollViewDelegate> _forwardResponder;
+    NSRange _range;
+    NSInteger _currentSelection;
+    id<ELPickerViewDelegate> _delegate;
+}
+@property(nonatomic, assign) id<ELPickerViewDelegate> delegate;
+
+- (id)initWithRange:(NSRange)range andFrame:(CGRect)frame;
+
+- (void)setRightPosition;
+
+- (void)setSelection:(NSInteger)shiftFromLocation;
+
+@end
