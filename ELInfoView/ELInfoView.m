33f7a3a7f68d8ecdc51ae8a436c356821d2a7b31
Yarik Smirnov <yaroslav.smirnov@e-legion.com>
2011-12-23 06:15:09 +0400

    remove useless code in ELInfoView

--- ELInfoView/ELInfoView.m
+++ ELInfoView/ELInfoView.m
@@ -124,30 +124,10 @@ static CGFloat const kHideAlphaValue = 0.0;
 }
 
 
-
-- (void)fillRoundedRect:(CGRect)rect inContext:(CGContextRef)context
-{
-    float radius = 5.0f;
-    
-    CGContextBeginPath(context);
-    CGContextSetRGBFillColor(context, 0,0,0,0.75);
-	CGContextMoveToPoint(context, CGRectGetMinX(rect) + radius, CGRectGetMinY(rect));
-    CGContextAddArc(context, CGRectGetMaxX(rect) - radius, CGRectGetMinY(rect) + radius, radius, 3 * M_PI / 2, 0, 0);
-    CGContextAddArc(context, CGRectGetMaxX(rect) - radius, CGRectGetMaxY(rect) - radius, radius, 0, M_PI / 2, 0);
-    CGContextAddArc(context, CGRectGetMinX(rect) + radius, CGRectGetMaxY(rect) - radius, radius, M_PI / 2, M_PI, 0);
-    CGContextAddArc(context, CGRectGetMinX(rect) + radius, CGRectGetMinY(rect) + radius, radius, M_PI, 3 * M_PI / 2, 0);
-	
-    CGContextClosePath(context);
-    CGContextFillPath(context);
-}
-
 - (void)drawRect:(CGRect)rect
 {
-	// draw a box with rounded corners to fill the view -
-//	CGRect boxRect = self.bounds;
     CGContextRef context = UIGraphicsGetCurrentContext();	
-//	boxRect = CGRectInset(boxRect, 1.0f, 1.0f);
-//    [self fillRoundedRect:boxRect inContext:ctxt];
+
     CGContextClearRect(context, rect);
     
     CGMutablePathRef path = CGPathCreateMutable();
