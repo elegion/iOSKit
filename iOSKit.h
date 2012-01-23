efda9bcec01c4c4cf8b24415afe1e9e553c10225
Yarik Smirnov <yaroslav.smirnov@e-legion.com>
2012-01-23 23:31:36 +0400

    Added general header for library

---
+++ iOSKit.h
@@ -0,0 +1,14 @@
+//
+//  iOSKit.h
+//  Ginza
+//
+//  Created by Yarik Smirnov on 1/23/12.
+//  Copyright (c) 2012 e-Legion ltd. All rights reserved.
+//
+
+#ifndef Ginza_iOSKit_h
+#define Ginza_iOSKit_h
+
+
+
+#endif
